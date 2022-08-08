121618603086b6b8b6f3a6f0ebc793c4eacc35fc
ponjae <ponjae11@gmail.com>
2022-08-08 13:44:18 +0200

    project done

--- monsters/src/components/card-list/card-list.component.jsx
+++ monsters/src/components/card-list/card-list.component.jsx
@@ -1,14 +1,16 @@
 import { Component } from "react";
 
+import Card from "../card/card.component.js";
+import "./card-list.styles.css";
+
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
-
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
       </div>
     );
   }
